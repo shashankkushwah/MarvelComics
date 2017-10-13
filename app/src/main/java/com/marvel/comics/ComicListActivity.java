fab0065ad5b3e5bf737cb6f4f0b48ff70aded525
shashankkushwah <shashankkushwah@gmail.com>
2017-10-13 22:04:38 +0100

    Initial setup
    add retrofit, okhttp, rxjava, and picasso dependencies

---
+++ app/src/main/java/com/marvel/comics/ComicListActivity.java
@@ -0,0 +1,13 @@
+package com.marvel.comics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComicListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comic_list);
+    }
+}
